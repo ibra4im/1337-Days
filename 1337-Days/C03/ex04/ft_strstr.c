c8c5d016441a560a2320d6f99a36feb8d6c99fca
brahem <brahemansari514@gmail.com>
2026-03-22 10:18:34 +0100

    init

---
+++ 1337-Days/C03/ex04/ft_strstr.c
@@ -0,0 +1,28 @@
+char    *ft_strstr(char *str,char *to_find)
+{
+    int i = 0 ;
+    int j = 0 ;
+    if (to_find[j] == '\0')
+    {
+        return (0);
+    }
+    while (str[i] != '\0')
+    {
+        while (str[i + j] == to_find[j] && to_find[j])
+        {
+            j++;
+            if (to_find[j] == '\0')
+            {
+                return &str[i];
+            }
+        }
+        i++;
+    }
+}
+#include <stdio.h>
+int main()
+{
+    char *str = "hello world";
+    char *tofind = "world";
+    printf("%s",ft_strstr(str,tofind));
+}
