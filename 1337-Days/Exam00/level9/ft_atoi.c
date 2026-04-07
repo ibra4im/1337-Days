93b5148dccb08b6464e8191b4af03a6fb8dcdc66
brahem <brahemansari514@gmail.com>
2026-04-07 13:40:28 +0100

    complete exam00

---
+++ 1337-Days/Exam00/level9/ft_atoi.c
@@ -0,0 +1,24 @@
+int ft_atoi(char *str)
+{
+    int sign = 1;
+    int res;
+    int i = 0;
+    while (str[i] <= 32)
+    {
+        i++;
+    }
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+        {
+            sign *= -1;
+        }
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    return res * sign;
+}
