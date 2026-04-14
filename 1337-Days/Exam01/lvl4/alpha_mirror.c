9d88b507168a219a21e2fbea9671868f6658f110
brahem <brahemansari514@gmail.com>
2026-04-14 11:50:47 +0100

    lvl5 exam 01

---
+++ 1337-Days/Exam01/lvl4/alpha_mirror.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+int main(int ac, char *av[])
+{
+    if (ac > 1)
+    {
+        int i = 0 ;        
+        while (av[1][i] != '\0')
+        {
+            if (av[1][i] >= 'a' && av[1][i] <= 'z')
+            {
+                av[1][i] =  'z' + 'a' - av[1][i];
+                write(1,&av[1][i],1);
+            }
+            else if (av[1][i] >= 'A' && av[1][i] >= 'Z')
+            {
+                av[1][i] =  'Z' + 'A' - av[1][i];
+                write(1,&av[1][i],1);            
+            }
+            else 
+                write(1,&av[1][i],1);
+            i++;
+        }
+    }
+    write(1,"\n",1);
+}
