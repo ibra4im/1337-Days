c9fdc6d0a4865542d59dd81b52ccac9dc8a7df88
bya_ <aminabya77@gmail.com>
2026-04-20 19:36:28 +0200

    Update fizzbuzz.c
    omi dzb

--- 1337-Days/Exam01/lvl4/fizzbuzz.c
+++ 1337-Days/Exam01/lvl4/fizzbuzz.c
@@ -16,20 +16,20 @@ void    ft_putnbr(int nb)
 }
 int main()
 {
-    int i = 0;
+    int i = 1;
     while (i <= 100)
     {
-        if (i % 3 == 0)
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            write(1,"fizzbuzz\n",9);
+        }
+        else if (i % 3 == 0)
         {
             write(1,"fizz\n",5);
         }
         else if (i % 5 == 0)
         {
             write(1,"buzz\n",5);
-        }
-        else if (i % 3 == 0 && i % 5 == 0)
-        {
-            write(1,"fizzbuzz\n",9);
         }
         else 
         {
