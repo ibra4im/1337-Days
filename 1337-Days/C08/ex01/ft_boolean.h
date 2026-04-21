223b259a70b6354d38199133eb6fefd39e720aad
brahem <brahemansari514@gmail.com>
2026-04-21 13:24:28 +0100

    leet

---
+++ 1337-Days/C08/ex01/ft_boolean.h
@@ -0,0 +1,17 @@
+#ifndef FT_BOOLEAN.H
+#define FT_BOOLEAN.H
+
+#include <unistd.h>
+#include <stdbool.h>
+
+#define EVEN(nbr) (nbr % 2 == 0)
+typedef t_bool
+
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+
+#define EVEN_MSG "I have an even number of arguments."
+#define  ODD_MSG "I have an odd number of arguments."
+ 
+#endif
